9515de7501cb005b230e155383ee0b9cd73401d1
aryaman-titan <aryamanbehera@gmail.com>
2020-02-16 15:42:42 +0530

    added tables

--- welcome.php
+++ welcome.php
@@ -1,5 +1,6 @@
 <?php
 include('server.php');
+include('config.php');
 session_start();
   if (!isset($_SESSION['id'])) {
   	header('location: index.php');
@@ -16,6 +17,23 @@ session_start();
 
 <html lang="en">
 <head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome <?php echo $_SESSION["name"];?></title>
@@ -26,6 +44,31 @@ Welcome <?php echo $_SESSION["name"];?>
 <div style="display: flex; justify-content: flex-end;"><a href="welcome.php?logout=1">Logout</a>
 </div>
 <hr>
+
+<div class="books-available">
+<table>
+  <tr>
+    <th>Book ID</th>
+    <th>Name</th>
+    <th>Copies Left</th>
+    <th>Copies Issued</th>
+  </tr>
+
+  <?php 
+    $get_books = mysqli_query($conn, "SELECT * FROM `book_list`");
+    while($row = $get_books->fetch_assoc()){
+      echo "<tr>
+      <td>".$row['id']."</td>
+      <td>".$row['book_name']."</td>
+      <td>".$row['book_count_left']."</td>
+      <td>".$row['book_count_issued']."</td>
+    </tr>";
+    }
+  ?>
+  
+  
+</table>
+</div>
 
 
 
