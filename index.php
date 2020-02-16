5e3859065147f66ddd3cc4a4339ab68eefbe30d8
aryaman-titan <aryamanbehera@gmail.com>
2020-02-16 13:58:46 +0530

    added server.php

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+
+<?php 
+include('server.php');
+?>
+
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
-  <meta charset="UTF-8"> 
-  <title>Library Management System</title>
-  <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <title>Library Management System</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Library Management System</title>
@@ -12,36 +18,40 @@
     <link rel="stylesheet" href="style.css">
 
 </head>
+
 <body>
 
-<div class="form-modal">
-    
-    <div class="form-toggle">
-        <button id="login-toggle" onclick="toggleLogin()">log in</button>
-        <button id="signup-toggle" onclick="toggleSignup()">sign up</button>
+    <div class="form-modal">
+
+        <div class="form-toggle">
+            <button id="login-toggle" onclick="toggleLogin()">log in</button>
+            <button id="signup-toggle" onclick="toggleSignup()">sign up</button>
+        </div>
+
+        <div id="login-form">
+            <form method="POST" action="index.php">
+                <input type="text" name="username" placeholder="Enter username or email ID" required />
+                <input type="password" name="password" placeholder="Enter password" required />
+                <button type="submit" class="btn login">login</button>
+            </form>
+        </div>
+
+        <div id="signup-form">
+            <form method="POST" action="index.php">
+                <input type="email" name="email" placeholder="Enter your email" required />
+                <input type="text" name="username" placeholder="Choose username" required />
+                <input type="text" name="name" placeholder="Enter your name" required />
+                <input type="text" name="enrl" placeholder="Enter your Enrl. No." required />
+                <input type="password" name="password" placeholder="Create password" required />
+                <button type="submit" name="signup" class="btn signup">create account</button>
+
+            </form>
+        </div>
+
     </div>
 
-    <div id="login-form">
-        <form method = "POST" action = "server.php">
-            <input type="text" name = "username" placeholder="Enter username or email ID" required/>
-            <input type="password" name = "password" placeholder="Enter password" required/>
-            <button type="submit" class="btn login">login</button>
-        </form>
-    </div>
-
-    <div id="signup-form">
-        <form method = "POST" action = "server.php">
-            <input type="email" name = "email" placeholder="Enter your email"/>
-            <input type="text" name = "username" placeholder="Choose username"/>
-            <input type="password" name = "password" placeholder="Create password"/>
-            <button type="submit" class="btn signup">create account</button>
-                       
-        </form>
-    </div>
-
-</div>
-
-  <script  src="script.js"></script>
+    <script src="script.js"></script>
 
 </body>
+
 </html>
--- server.php
+++ server.php
@@ -1,8 +1,30 @@
 <?php
-//connecting to the database
+
 
 include('config.php');
 
 
+//connecting to the database
 
-?>
+
+
+if (isset($_POST["signup"])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $name =  mysqli_real_escape_string($conn, $_POST['name']);
+    $username =  mysqli_real_escape_string($conn, $_POST['username']);
+    $password =  mysqli_real_escape_string($conn, $_POST['password']);
+    $enrl =  mysqli_real_escape_string($conn, $_POST['enrl']);
+
+    $register = "INSERT INTO `userData` (`userID`, `username`, `name`, `enrl`, `password`, `email`) VALUES (NULL, '$username', '$name', '$enrl', '$password', '$email')";
+
+    $get_user = "SELECT * FROM userData WHERE username='$username'";
+    $result = mysqli_query($db, $get_user);
+
+    if (mysqli_num_rows($result) == 1) {
+        while ($row = mysqli_fetch_array($result)) {
+            $_SESSION['id'] = $row['userID'];
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['username'] = $row['username'];
+        }
+    }
+}
